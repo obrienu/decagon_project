0440796fc173723446fbe39fbf15b5f4a9e84a60
obrienu <obrienules@gmail.com>
2019-09-17 12:13:03 +0100

    Create a get and post function to handle staff registration

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,21 @@
 $(document).ready(() => {
+  //const postRoute = include('./modules/ajas');
+  function postRoute(key, data) {
+    return $.ajax({
+      url: `http://localhost:3000/${key}`,
+      method: 'POST',
+      data: data,
+      'Content-Type': 'application / json',
+      success: function(data) {
+        $('#notice').html('Registration Successful');
+        console.log('success');
+      },
+      error: function(e) {
+        $('#notice').html('Staff ID already registered');
+      }
+    });
+  }
+
   let tabs = $('.show-tab');
   let tabContent = $('.tab-content');
 
@@ -14,10 +31,35 @@ $(document).ready(() => {
     removeSelected();
     removeShow();
     const tabContentItem = $(`#${e.currentTarget.id}-content`);
-    console.log(e.currentTarget.id);
     tabContentItem.addClass('show');
     $(this).addClass('selected');
   }
 
   tabs.on('click', selectItem);
+
+  /* *************Handle Login and Sign up*********** */
+  $('#sign-up').on('submit', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: `http://localhost:3000/staffID`,
+      type: 'GET',
+      success: function(data) {
+        let name = $('#name').val();
+        let id = $('#staff-id1').val();
+        let email = $('#email').val();
+        let password = $('#password').val();
+        let key = 'profile';
+        let body = { id, name, email, password };
+        if (data.includes(id)) {
+          postRoute(key, body);
+        } else {
+          $('#notice').html('Invalid Staff ID');
+        }
+      },
+      error: function(e) {
+        $('#notice').html('Staff ID already registered');
+      }
+    });
+  });
 });
