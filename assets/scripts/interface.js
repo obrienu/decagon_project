e9028f6a736f31add466ac73052d6d5f3ddd8cff
obrienu <obrienules@gmail.com>
2019-09-18 23:01:39 +0100

    refactored interface.js to generate unique pin for each denomination based on first two digit

--- assets/scripts/interface.js
+++ assets/scripts/interface.js
@@ -1,41 +1,5 @@
 $(document).ready(function() {
-  /* async function postRoute(key, data) {
-    try {
-      let result = await $.ajax({
-        url: `http://localhost:3000/${key}`,
-        method: 'POST',
-        data: data,
-        'Content-Type': 'application / json',
-        success: function(data) {
-          $('#notice').html('Registration Successful');
-        }
-      });
-      return result;
-    } catch (error) {
-      console.error(error);
-    }
-  } */
-
-  async function generateCard() {
-    try {
-      let num = Math.floor(Math.random() * 1000000000000000) + 3000000000000000;
-      let denomination = ['100', '200', '400', '500', '750', '1000'];
-      let val;
-      for (let i = 0; i < denomination.length; i++) {
-        val = await $.ajax({
-          url: `http://localhost:3000/${denomination[i]}?value=${num}`,
-          type: 'GET'
-        });
-        if (val.includes(num)) {
-          generateCard();
-        }
-      }
-      return num;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  //console.log(generateCard());
+  //*********GET USER INFO */
   let link = window.location.href;
   link.indexOf('=');
   let user = link.substring(link.indexOf('=') + 1, link.length);
@@ -43,26 +7,72 @@ $(document).ready(function() {
 
   $('form').on('submit', async function(e) {
     e.preventDefault();
-    let quantity = $('#quantity').val();
-    let staff = $('#user').val();
-    let validity = 'Valid';
-    let key = $('#drop').val();
 
+    /*****FUNCTION TO GENERATE PIN BASED ON SPECIFIED DENOMINATION */
+
+    function generatePin() {
+      let denomination = $('#drop').val();
+      switch (denomination) {
+        case '100':
+          return Math.floor(Math.random() * 10000000000000) + 3100000000000000;
+          break;
+        case '200':
+          return Math.floor(Math.random() * 10000000000000) + 3200000000000000;
+          break;
+        case '400':
+          return Math.floor(Math.random() * 10000000000000) + 3400000000000000;
+          break;
+        case '500':
+          return Math.floor(Math.random() * 10000000000000) + 3500000000000000;
+          break;
+        case '750':
+          return Math.floor(Math.random() * 10000000000000) + 3700000000000000;
+          break;
+        case '1000':
+          return Math.floor(Math.random() * 10000000000000) + 3000000000000000;
+          break;
+      }
+    }
+
+    /******FUNCTION CHECKS FOR PIN IN DATABASE AND ADDS IF ABSENT */
+
+    function generateCard() {
+      let num = generatePin();
+      let key = $('#drop').val();
+      try {
+        $.ajax({
+          url: `http://localhost:3000/${key}?num=${num}`,
+          method: 'GET',
+          async: false,
+          success: res => {
+            if (res.length === 0) {
+              try {
+                let staff = $('#user').val();
+                let validity = 'Valid';
+                $.ajax({
+                  url: `http://localhost:3000/${key}`,
+                  method: 'POST',
+                  data: { num, validity, staff },
+                  async: false,
+                  success: res => {
+                    console.log('success');
+                  }
+                });
+              } catch (error) {
+                console.log(error);
+              }
+            }
+          }
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    //*********GENERATE SPECIFIED QUANTITY */
+    let quantity = $('#quantity').val();
     for (let i = 0; i < Number(quantity); i++) {
-      generateCard().then(num => {
-        try {
-          $.ajax({
-            url: `http://localhost:3000/${key}`,
-            method: 'POST',
-            data: { num, validity, staff },
-            'Content-Type': 'application / json'
-          });
-        } catch (error) {
-          console.error(error);
-        }
-      });
-      //body = { num, validity, staff };
-      // await postRoute(denom, body);
+      generateCard();
     }
   });
 });
