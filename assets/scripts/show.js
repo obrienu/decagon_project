bf69bb89cc2cf4af9c0d38a28b8614c4b5ea9b08
obrienu <obrienules@gmail.com>
2019-09-18 01:23:08 +0100

    Add show.js and show.html, displays all recharge card in the database

---
+++ assets/scripts/show.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  async function show(key, val) {
+    let result;
+
+    try {
+      if (val === 'all') {
+        result = await $.ajax({
+          url: `http://localhost:3000/${key}`,
+          type: 'GET'
+          //  data: data
+        });
+        return result;
+      } else {
+        result = await $.ajax({
+          url: `http://localhost:3000/${key}?validity=${val}`,
+          type: 'GET'
+          //  data: data
+        });
+      }
+
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  $('#show-page').on('submit', async e => {
+    e.preventDefault();
+    let key = $('#drop').val();
+    let validity = $('#validity').val();
+    let data = await show(key, validity);
+    $('.list').remove();
+    $('#denominaton').text(`${key}`);
+    for (let i = 0; i < data.length; i++) {
+      $('#display').prepend(
+        `<div class="col-md-4 col-lg-3" id="section"> <li class="list"><a id="${
+          data[i]['id']
+        }" class="link" href="#"> ${
+          data[i]['num']
+        }</a> </li> <li class="list">Validity: ${
+          data[i]['validity']
+        }</li> </div> `
+      );
+    }
+  });
+});
